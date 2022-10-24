92cde9963ea7fb98a84037041e81846b1fc91095
Joairazabal <95320625+Joairazabal@users.noreply.github.com>
2022-10-24 11:51:16 -0300

    ready

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+//ejercicio 1
+
+Console.WriteLine("your firsname:");
+string firsname = Console.ReadLine();
+Console.WriteLine("your lastname:");
+string lastName = Console.ReadLine();
+Console.WriteLine("your expriencie:");
+bool experiencie = "si" == Console.ReadLine();
+Console.WriteLine( firsname + " " + lastName + " " + experiencie);
+
+//ejercicio 2
+
+//coche
+int puertas;
+int ruedas;
+string marca;
+int ITV;
+
+//mesa
+float peso;
+float largo;
+string material;
+string color;
+
+//ejercicio 3
+string num= "un numero >= 18";
+string unChar= "un char === a";
+bool boleano= 1 > 0 && 1 < 1;
+bool boleano2= 1 > 0 || 1>1;
